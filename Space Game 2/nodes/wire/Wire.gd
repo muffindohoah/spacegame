4b0f69532a7f7897b2e92037ab2fc8e089b9cf0c
muffindoh <uwufunkytown@gmail.com>
2024-09-16 23:05:05 -0500

    YUMMERS

--- Space Game 2/nodes/wire/Wire.gd
+++ Space Game 2/nodes/wire/Wire.gd
@@ -4,9 +4,8 @@ var WiredFrom
 var WiredTo
 var WireColor = Color(0,25,200)
 
-
 func _ready():
 	default_color = WireColor
-	add_point(WiredTo.global_position)
 	add_point(WiredFrom.global_position)
+	add_point(WiredTo.global_position)
 
